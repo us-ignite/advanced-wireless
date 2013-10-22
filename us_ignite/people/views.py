9a78f19038fe0c03c6d640f85d9abb050aa48e0d
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-10-22 15:35:41 +0100

    Improve support for ordering people profiles.
    https://github.com/madewithbytes/us_ignite/issues/47
    
    The profiles can be sorted by using the form embeded.

--- us_ignite/people/views.py
+++ us_ignite/people/views.py
@@ -2,14 +2,24 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from us_ignite.profiles.models import Profile
-from us_ignite.common import pagination
+from us_ignite.common import pagination, forms
+
+
+PROFILE_SORTING_CHOICES = (
+    ('', 'Select ordering'),
+    ('user__first_name', 'First name a-z'),
+    ('-user__first_name', 'First name z-a'),
+    ('user__last_name', 'Last name a-z'),
+    ('-user__last_name', 'Last name z-a'),
+)
 
 
 @login_required
 def people_list(request):
     page_no = pagination.get_page_no(request.GET)
-    order_value = pagination.get_order_value(
-        request.GET, ['user__first_name', 'user__last_name'])
+    order_form = forms.OrderForm(
+        request.GET, order_choices=PROFILE_SORTING_CHOICES)
+    order_value = order_form.cleaned_data['order'] if order_form.is_valid() else ''
     object_list = Profile.active.all()
     if order_value:
         object_list = object_list.order_by(order_value)
@@ -17,5 +27,6 @@ def people_list(request):
     context = {
         'page': page,
         'order': order_value,
+        'order_form': order_form,
     }
     return render(request, 'people/object_list.html', context)
