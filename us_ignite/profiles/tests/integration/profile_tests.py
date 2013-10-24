7f0109fb4cb5554c43dc97dfdb32532262589cfe
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-10-24 22:54:44 +0100

    Add tests for the Link inline form in the Profile.

--- us_ignite/profiles/tests/integration/profile_tests.py
+++ us_ignite/profiles/tests/integration/profile_tests.py
@@ -6,11 +6,12 @@ from registration.models import RegistrationProfile
 from nose.tools import eq_
 
 from us_ignite.common.tests import utils
+from us_ignite.profiles.models import Profile, ProfileLink
 from us_ignite.profiles.tests import fixtures
 
 
 def _teardown_profiles():
-    for model in [RegistrationProfile, User]:
+    for model in [RegistrationProfile, Profile, User]:
         model.objects.all().delete()
 
 
@@ -31,10 +32,31 @@ class TestUnauthenticatedEditProfilePage(TestCase):
         assert_redirects(response, utils.get_login_url(url))
 
 
+def _get_profilelink_inline_payload(profile_id, data_list=None, **kwargs):
+    """Generates an inline form to be passed as part of the payload."""
+    data_list = data_list if data_list else [{}]
+    prefix = 'profilelink_set-'
+    default = {
+        '%sTOTAL_FORMS' % prefix: len(data_list),
+        '%sINITIAL_FORMS' % prefix: 0,
+        '%sMAX_NUM_FORMS'% prefix: 3,
+    }
+    for i, d in enumerate(data_list):
+        default.update({
+            '%s%s-name' % (prefix, i): d.get('name', ''),
+            '%s%s-url' % (prefix, i): d.get('url', ''),
+            '%s%s-DELETE' % (prefix, i): d.get('DELETE', ''),
+            '%s%s-id' % (prefix, i): d.get('id', ''),
+            '%s%s-profile' % (prefix, i): profile_id,
+        })
+    default.update(kwargs)
+    return default
+
+
 class TestEditProfilePage(TestCase):
 
     def setUp(self):
-        fixtures.get_user('us-ignite', email='user@us-ignite.org')
+        self.user = fixtures.get_user('us-ignite', email='user@us-ignite.org')
         self.client.login(username='us-ignite', password='us-ignite')
 
     def tearDown(self):
@@ -48,24 +70,36 @@ class TestEditProfilePage(TestCase):
         eq_(sorted(fields), ['bio', 'first_name', 'last_name', 'website'])
 
     def test_profile_form_update_is_successful(self):
+        profile, is_new = Profile.objects.get_or_create(user=self.user)
         url = '/accounts/profile/'
-        data = {
-            'first_name': 'John',
-            'last_name': 'Donne',
-        }
+        data = {'first_name': 'John', 'last_name': 'Donne'}
+        data.update(_get_profilelink_inline_payload(profile.pk))
         response = self.client.post(url, data)
         assert_redirects(response, '/accounts/profile/')
         values = (User.objects.values('first_name', 'last_name')
                   .get(username='us-ignite'))
-        eq_(values, data)
+        eq_(values, {'first_name': 'John', 'last_name': 'Donne'})
 
     def test_profile_ignores_invaid_values(self):
+        profile, is_new = Profile.objects.get_or_create(user=self.user)
         url = '/accounts/profile/'
-        data = {
-            'email': 'invalid@us-ignite.org',
-        }
+        data = {'email': 'invalid@us-ignite.org'}
+        data.update(_get_profilelink_inline_payload(profile.pk))
         response = self.client.post(url, data)
         assert_redirects(response, '/accounts/profile/')
         values = (User.objects.values('email')
                   .get(username='us-ignite'))
         eq_(values, {'email': 'user@us-ignite.org'})
+
+    def test_profile_updates_inline_models(self):
+        profile, is_new = Profile.objects.get_or_create(user=self.user)
+        url = '/accounts/profile/'
+        data = {'first_name': '', 'last_name': ''}
+        data_list = [{'name': 'Github', 'url': 'http://github.com/'}]
+        data.update(_get_profilelink_inline_payload(
+            profile.pk, data_list=data_list))
+        response = self.client.post(url, data)
+        assert_redirects(response, '/accounts/profile/')
+        values = list(ProfileLink.objects.values('name', 'url')
+                      .filter(profile=profile))
+        eq_(values, [{'name': 'Github', 'url': 'http://github.com/'}])
