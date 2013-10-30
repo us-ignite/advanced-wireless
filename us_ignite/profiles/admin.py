6e46bef6f365d9db6b906395704bf0beca751da8
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-10-30 15:33:20 +0000

    Provide mechanics to invite users in batches.
    https://github.com/madewithbytes/us_ignite/issues/18
    
    An admin form is available to import users in batch,
    the format of the field must be a new line for
    each user and each user should be in the following
    format::
    
        name, email
    
    It will use similar mechanics to ``browserid`` to
    create and invite the users.
    
    Closes #18.

--- us_ignite/profiles/admin.py
+++ us_ignite/profiles/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.conf.urls import patterns, url
+from django.shortcuts import render, redirect
 
 from us_ignite.profiles.models import Profile, ProfileLink
+from us_ignite.profiles import inviter, forms
+
 
 email = lambda u: u.user.email
 email.short_description = 'email'
@@ -11,9 +15,47 @@ class ProfileLinkInline(admin.TabularInline):
 
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = (email, 'slug')
-    search_fields = ('user__first_name', 'slug')
+    list_display = (email, 'display_name', 'slug')
+    search_fields = ('user__first_name', 'slug', 'display_name')
     inlines = [ProfileLinkInline]
+
+    def get_urls(self):
+        _invite_users = self.admin_site.admin_view(self.invite_users)
+        urls = patterns(
+            '',
+            url(r'^inviter/$', _invite_users, name='invite_users'),
+        )
+        urls += super(ProfileAdmin, self).get_urls()
+        return urls
+
+    def _notify_results(self, request, results):
+        if results:
+            message = '%s users were imported correctly.' % len(results)
+        else:
+            message = 'No new users were imported.'
+        self.message_user(request, message)
+
+    def invite_users(self, request):
+        """Invites a list of provided users.
+
+        Each new user should be in a different line and the format
+        of each row is::
+
+            name, email
+        """
+        if request.method == 'POST':
+            form = forms.InviterForm(request.POST)
+            if form.is_valid():
+                results = inviter.invite_users(form.cleaned_data['users'])
+                self._notify_results(request, results)
+                return redirect('admin:profiles_profile_changelist')
+        else:
+            form = forms.InviterForm()
+        context = {
+            'form': form,
+            'title': 'Invite users',
+        }
+        return render(request, 'admin/profiles/inviter.html', context)
 
 
 admin.site.register(Profile, ProfileAdmin)
---
+++ us_ignite/profiles/tests/integration/admin_tests.py
@@ -0,0 +1,100 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from django_nose.tools import assert_redirects
+from registration.models import RegistrationProfile
+from nose.tools import eq_, ok_
+
+
+from us_ignite.profiles.models import Profile
+from us_ignite.profiles.tests import fixtures
+
+
+def _get_payload(user_list):
+    users = []
+    for user in user_list:
+        users.append(','.join(user))
+    return {'users': '\n'.join(users)}
+
+
+def _teardown_profiles():
+    for model in [RegistrationProfile, Profile, User]:
+        model.objects.all().delete()
+
+
+def assert_admin_login(response):
+    eq_(response.status_code, 200)
+    ok_('id_username' in response.content)
+    ok_('id_password' in response.content)
+
+
+class TestUnauthenticatedInviteAdmin(TestCase):
+
+    def test_import_users_require_admin(self):
+        url = '/admin/profiles/profile/inviter/'
+        response = self.client.get(url)
+        assert_admin_login(response)
+
+    def test_import_post_payload_require_admin(self):
+        url = '/admin/profiles/profile/inviter/'
+        payload = _get_payload([('Alpha', 'alpha@us-ignite.org')])
+        response = self.client.post(url, payload)
+        assert_admin_login(response)
+
+
+class TestRegularUserInviteAdmin(TestCase):
+
+    def setUp(self):
+        self.user = fixtures.get_user('us-ignite', email='user@us-ignite.org')
+        self.client.login(username='us-ignite', password='us-ignite')
+
+    def tearDown(self):
+        self.client.logout()
+        _teardown_profiles()
+
+    def test_import_users_require_admin(self):
+        url = '/admin/profiles/profile/inviter/'
+        response = self.client.get(url)
+        assert_admin_login(response)
+
+    def test_import_post_payload_require_admin(self):
+        url = '/admin/profiles/profile/inviter/'
+        payload = _get_payload([('Alpha', 'alpha@us-ignite.org')])
+        response = self.client.post(url, payload)
+        assert_admin_login(response)
+
+
+class TestInviteAdmin(TestCase):
+
+    def setUp(self):
+        self.user = fixtures.get_user('us-ignite', email='user@us-ignite.org')
+        self.user.is_superuser = True
+        self.user.is_staff = True
+        self.user.save()
+        self.client.login(username='us-ignite', password='us-ignite')
+
+    def tearDown(self):
+        self.client.logout()
+        _teardown_profiles()
+
+    def test_get_request_is_successful(self):
+        url = '/admin/profiles/profile/inviter/'
+        response = self.client.get(url)
+        eq_(response.status_code, 200)
+        ok_(response.content, 'id_users')
+
+    def test_post_empty_payload_fails(self):
+        url = '/admin/profiles/profile/inviter/'
+        response = self.client.post(url, {})
+        eq_(response.status_code, 200)
+        ok_(response.content, 'errors')
+
+    def test_post_valid_payload_succeds(self):
+        url = '/admin/profiles/profile/inviter/'
+        payload = _get_payload([('Alpha', 'alpha@us-ignite.org')])
+        response = self.client.post(url, payload)
+        assert_redirects(response, '/admin/profiles/profile/')
+        user = User.objects.get(email='alpha@us-ignite.org')
+        ok_(user.username)
+        eq_(user.profile.display_name, 'Alpha')
+        ok_(user.profile.slug)
