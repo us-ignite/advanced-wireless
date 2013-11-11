67d7ce2d9e8ffe26f5f5a97aca9cfb99c8914f3e
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-11 13:04:38 +0000

    Add ``TestMessagesBackend`` for testing ``django.contrib.messages``.
    When unit testing a django view the ``messages`` middleware
    will be missing. This backend will provision a simple
    messaging midleware.
    
    Usage::
    
        from django.test import client
        from us_ignite.common.tests import utils
    
        factory = client.RequestFactory()
        request = factory.get('/')
        request._messages = utils.TestMessagesBackend(request)
    
    The messages will be added to this simple backend.

--- us_ignite/common/tests/utils.py
+++ us_ignite/common/tests/utils.py
@@ -1,6 +1,17 @@
 from django.core.urlresolvers import reverse
+from django.contrib.messages.storage.base import BaseStorage, Message
 
 
 def get_login_url(url):
     """Returns an expected login URL."""
     return ('%s?next=%s' % (reverse('auth_login'), url))
+
+
+class TestMessagesBackend(BaseStorage):
+    def __init__(self, request, *args, **kwargs):
+        self._loaded_data = []
+        super(TestMessagesBackend, self).__init__(request, *args, **kwargs)
+
+    def add(self, level, message, extra_tags=''):
+        self._loaded_data.append(
+            Message(level, message, extra_tags=extra_tags))
