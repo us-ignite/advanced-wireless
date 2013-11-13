7cff5baf183c7d931c0b17a9ec61a1768f803fd6
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-13 18:24:47 +0000

    Added usage docstring to messages helper for testing.

--- us_ignite/common/tests/utils.py
+++ us_ignite/common/tests/utils.py
@@ -8,6 +8,20 @@ def get_login_url(url):
 
 
 class TestMessagesBackend(BaseStorage):
+    """
+    When unit testing a django view the ``messages`` middleware
+    will be missing. This backend will provision a simple
+    messaging midleware.
+
+    Usage::
+
+    from django.test import client
+    from us_ignite.common.tests import utils
+
+    factory = client.RequestFactory()
+    request = factory.get('/')
+    request._messages = utils.TestMessagesBackend(request)
+    """
     def __init__(self, request, *args, **kwargs):
         self._loaded_data = []
         super(TestMessagesBackend, self).__init__(request, *args, **kwargs)
