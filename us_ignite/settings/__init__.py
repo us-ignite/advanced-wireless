541f727726bea79c89eb1d47be3847a8811fb0ac
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-20 18:40:20 +0000

    Amend ``MEDIA_ROOT`` to upload files at the root of the repo.

--- us_ignite/settings/__init__.py
+++ us_ignite/settings/__init__.py
@@ -45,7 +45,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = here('media')
+MEDIA_ROOT = here('..', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
