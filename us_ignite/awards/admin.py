a1e7cade8fb5e4a301244a8a0cef382b49399d46
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-21 12:31:11 +0000

    Add mechanics to award applications.
    https://github.com/madewithbytes/us_ignite/issues/77
    
    The awards can be given in the admin section.

--- us_ignite/awards/admin.py
+++ us_ignite/awards/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from us_ignite.awards.models import Award
+from us_ignite.awards.models import Award, ApplicationAward
 
 
 class AwardAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug')
 
+
+class ApplicationAwardAdmin(admin.ModelAdmin):
+    list_display = ('award', 'application', 'created')
+    date_hierarchy = 'created'
+    list_filter = ('award__name',)
+
 admin.site.register(Award, AwardAdmin)
+admin.site.register(ApplicationAward, ApplicationAwardAdmin)
