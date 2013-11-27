4c45a7635664a210e037b98329eaa8cb2cd2faab
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-27 21:25:46 +0000

    Add factory to generate ``hubs`` fixtures.
    The fixtures are helpful when DB objects are required.

--- us_ignite/hubs/tests/fixtures.py
+++ us_ignite/hubs/tests/fixtures.py
@@ -1,10 +1,22 @@
-from us_ignite.hubs.models import Hub
+from us_ignite.hubs.models import Hub, HubRequest
+from us_ignite.profiles.tests.fixtures import get_user
 
 
-def create_hub(**kwargs):
+def get_hub(**kwargs):
     data = {
         'name': 'Local Community',
         'description': 'Gigabit local community',
     }
     data.update(kwargs)
     return Hub.objects.create(**data)
+
+
+def get_hub_request(**kwargs):
+    data = {
+        'name': 'Local Community',
+        'description': 'Gigabit local community',
+    }
+    if not 'user' in kwargs:
+        data['user'] = get_user('user-community')
+    data.update(**kwargs)
+    return HubRequest.objects.create(**data)
