5ff393b60ac9fb89f061e64bff6393f9f2c9a89f
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-27 21:24:30 +0000

    List ``HubRequest`` in the admin section.

--- us_ignite/hubs/admin.py
+++ us_ignite/hubs/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
-from us_ignite.hubs.models import Hub, HubActivity
+from us_ignite.hubs.models import Hub, HubActivity, HubRequest
+
+
+class HubRequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created')
+    search_fields = ('name', 'description')
+    date_hierarchy = 'created'
+    list_filter = ('status', 'created')
 
 
 class HubAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'status', 'created')
-    search_fields = ('name', 'slug', 'description', 'features')
+    search_fields = ('name', 'slug', 'description', 'summary')
     date_hierarchy = 'created'
     list_filter = ('status', 'created')
 
@@ -16,5 +23,7 @@ class HubActivityAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
     list_filter = ('created', )
 
+
 admin.site.register(Hub, HubAdmin)
 admin.site.register(HubActivity, HubActivityAdmin)
+admin.site.register(HubRequest, HubRequestAdmin)
