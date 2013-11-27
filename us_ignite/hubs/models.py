f3000d7a2e87ad9951fadacde5709e548e23358f
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-27 17:12:08 +0000

    Add ``HubRequest`` model for apply-to-be-a-community functionality.
    https://github.com/madewithbytes/us_ignite/issues/94
    
    The model will be used to track the application process.

--- us_ignite/hubs/models.py
+++ us_ignite/hubs/models.py
@@ -5,15 +5,42 @@ from django_extensions.db.fields import (
 from taggit.managers import TaggableManager
 
 
-class Hub(models.Model):
-    """Local communities with Gigabit capabilities."""
+class HubRequest(models.Model):
     APPROVED = 1
     PENDING = 2
-    REMOVED = 3
+    REJECTED = 3
+    REMOVED = 4
     STATUS_CHOICES = (
         (APPROVED, 'Approved'),
         (PENDING, 'Pending'),
+        (REJECTED, 'Rejected'),
         (REMOVED, 'Removed'),
+    )
+    name = models.CharField(max_length=255)
+    website = models.URLField(max_length=500, blank=True)
+    summary = models.TextField(blank=True)
+    description = models.TextField()
+    user = models.ForeignKey('auth.User')
+    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+    notes = models.TextField(
+        blank=True, help_text='These notes will be feedback to the applicant.')
+    created = CreationDateTimeField()
+    modified = ModificationDateTimeField()
+
+    def __unicode__(self):
+        return u'%s by %s' % (self.name, self.user)
+
+    class Meta:
+        ordering = ('created', )
+
+
+class Hub(models.Model):
+    """Local communities with Gigabit capabilities."""
+    PUBLISHED = 1
+    DRAFT = 2
+    STATUS_CHOICES = (
+        (PUBLISHED, 'Published'),
+        (DRAFT, 'Draft'),
     )
     name = models.CharField(max_length=255)
     slug = AutoSlugField(populate_from='name', unique=True)
@@ -25,7 +52,7 @@ class Hub(models.Model):
     features = models.ManyToManyField('apps.Feature', blank=True)
     tags = TaggableManager(blank=True)
     notes = models.TextField(blank=True)
-    status = models.IntegerField(choices=STATUS_CHOICES, default=PENDING)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=DRAFT)
     is_featured = models.BooleanField(default=False)
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
--- us_ignite/hubs/tests/models_tests.py
+++ us_ignite/hubs/tests/models_tests.py
@@ -1,9 +1,35 @@
 from nose.tools import eq_, ok_
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from us_ignite.hubs.models import Hub, HubActivity
+from us_ignite.profiles.tests.fixtures import get_user
+from us_ignite.hubs.models import Hub, HubActivity, HubRequest
 from us_ignite.hubs.tests import fixtures
+
+
+class TestHubRequestModel(TestCase):
+
+    def tearDown(self):
+        for model in [HubRequest, User]:
+            model.objects.all().delete()
+
+    def test_model_can_be_created_successfully(self):
+        user = get_user('us-ignite')
+        data = {
+            'name': 'Local Community',
+            'description': 'Gigabit local community',
+            'user': user,
+        }
+        instance = HubRequest.objects.create(**data)
+        ok_(instance.id)
+        eq_(instance.name, 'Local Community')
+        eq_(instance.user, user)
+        eq_(instance.summary, '')
+        eq_(instance.description, 'Gigabit local community')
+        eq_(instance.website, '')
+        ok_(instance.created)
+        ok_(instance.modified)
 
 
 class TestHubModel(TestCase):
@@ -26,6 +52,8 @@ class TestHubModel(TestCase):
         eq_(instance.description, 'Gigabit local community')
         eq_(instance.image, '')
         eq_(instance.website, '')
+        ok_(instance.created)
+        ok_(instance.modified)
         eq_(list(instance.features.all()), [])
         eq_(list(instance.tags.all()), [])
 
