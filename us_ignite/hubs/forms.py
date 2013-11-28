8ae4468926c8317ec7a96105e1f681f6cb04ec8e
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-11-28 18:15:50 +0000

    Add form to edit a ``Hub``.
    https://github.com/madewithbytes/us_ignite/issues/97
    
    The Hubs will be edited by the guardian.

--- us_ignite/hubs/forms.py
+++ us_ignite/hubs/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 
-from us_ignite.hubs.models import HubRequest
+from us_ignite.hubs.models import Hub, HubRequest
 
 
 class HubRequestForm(forms.ModelForm):
 
     class Meta:
         model = HubRequest
+        fields = ('name', 'website', 'summary', 'description')
+
+
+class HubForm(forms.ModelForm):
+
+    class Meta:
+        model = Hub
         fields = ('name', 'website', 'summary', 'description')
--- us_ignite/hubs/tests/forms_tests.py
+++ us_ignite/hubs/tests/forms_tests.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from us_ignite.hubs import forms
 
 
-class TestHubForm(TestCase):
+class TestHubRequestForm(TestCase):
 
     def test_form_fields_are_not_sensitive(self):
         form = forms.HubRequestForm()
@@ -22,4 +22,24 @@ class TestHubForm(TestCase):
             'description': 'Community description.',
         }
         form = forms.HubRequestForm(data)
+        eq_(form.is_valid(), True)
+
+
+class TestHubForm(TestCase):
+
+    def test_form_fields_are_not_sensitive(self):
+        form = forms.HubForm()
+        eq_(sorted(form.fields.keys()),
+            sorted(['name', 'website', 'summary', 'description']))
+
+    def test_empty_form_fails(self):
+        form = forms.HubForm({})
+        eq_(form.is_valid(), False)
+
+    def test_valid_data_is_successful(self):
+        data = {
+            'name': 'Gigabit community',
+            'description': 'Community description.',
+        }
+        form = forms.HubForm(data)
         eq_(form.is_valid(), True)
