4581f1a9da0ac8129e84c4d65cf8f8514e7a2d7e
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-12-01 20:55:04 +0000

    Implement Ignite community approval flow.
    https://github.com/madewithbytes/us_ignite/issues/101
    
    Once the user has sent an request to be an Ignite Community
    the administrators will receive a notification of the request.
    
    The request will be resolved in the admin section, on the top
    right corner there is a link got resolve the request.
    
    The form receives only two values: ``status`` and ``notes``.
    
    - ``status`` determines the resolution of the request
    - ``notes`` an optional field that will be feedback to the
    user that generated the request.
    
    If the request has been marked as ``approved`` the view
    will generate a ``Hub`` where the ``user is the ``guardian``
    and associate the request with it.

--- us_ignite/hubs/admin.py
+++ us_ignite/hubs/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
+from django.conf.urls import patterns, url
+from django.shortcuts import redirect
+from django.http import Http404
+from django.template.response import TemplateResponse
 
+from us_ignite.hubs.forms import HubApprovalRequestForm
 from us_ignite.hubs.models import Hub, HubActivity, HubRequest
+
+
+def get_hub_from_request(instance):
+    """Transform the instance in a request"""
+    return Hub.objects.create(
+        name=instance.name,
+        guardian=instance.user,
+        summary=instance.summary,
+        description=instance.description,
+        website=instance.website
+    )
 
 
 class HubRequestAdmin(admin.ModelAdmin):
@@ -8,6 +24,47 @@ class HubRequestAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     date_hierarchy = 'created'
     list_filter = ('status', 'created')
+
+    def get_urls(self):
+        _approve_request = self.admin_site.admin_view(self.approve_request)
+        urls = patterns(
+            '',
+            url(r'^approve/(?P<request_id>\d+)/$', _approve_request,
+                name='approve_hub_request')
+        )
+        urls += super(HubRequestAdmin, self).get_urls()
+        return urls
+
+    def approve_request(self, request, request_id):
+        """Aprove the user request to own a community."""
+        try:
+            instance = HubRequest.objects.get(id=request_id)
+        except HubRequest.DoesNotExist:
+            raise Http404
+        # Determine if this request has been processed:
+        if not instance.is_pending():
+            raise Http404
+        if request.method == 'POST':
+            form = HubApprovalRequestForm(request.POST, instance=instance)
+            if form.is_valid():
+                instance = form.save()
+                # Process the request approval.
+                if instance.is_approved():
+                    hub = get_hub_from_request(instance)
+                    instance.hub = hub
+                    instance.save()
+                self.message_user(request, 'Hub request has been updated.')
+                return redirect(
+                    'admin:hubs_hubrequest_change', request_id)
+        else:
+            form = HubApprovalRequestForm(instance=instance)
+        context = {
+            'object': instance,
+            'form': form,
+            'title': 'Resolve Ignite community request',
+        }
+        return TemplateResponse(
+            request, 'admin/hubs/request_approval.html', context)
 
 
 class HubAdmin(admin.ModelAdmin):
---
+++ us_ignite/hubs/tests/admin_tests.py
@@ -0,0 +1,94 @@
+from nose.tools import raises, eq_, ok_
+from mock import patch, Mock
+
+from django.contrib.auth.models import User
+from django.contrib.admin.sites import AdminSite
+from django.http import Http404
+from django.test import client, TestCase
+
+from us_ignite.common.tests import utils
+from us_ignite.hubs.admin import HubRequestAdmin, get_hub_from_request
+from us_ignite.hubs.models import HubRequest, Hub
+from us_ignite.hubs.tests import fixtures
+
+
+patch_request_get = patch('us_ignite.hubs.models.HubRequest.objects.get')
+
+
+class HubRequestAdminTest(TestCase):
+
+    def setUp(self):
+        self.factory = client.RequestFactory()
+        self.site = AdminSite()
+
+    def _tear_down(self):
+        for model in [HubRequest, Hub]:
+            model.objects.all().delete()
+
+    @raises(Http404)
+    def test_approve_request_does_not_exists_fails(self):
+        admin = HubRequestAdmin(HubRequest, self.site)
+        request = self.factory.get('/')
+        admin.approve_request(request, 1)
+
+    @raises(Http404)
+    @patch_request_get
+    def test_request_has_been_approved_fails(self, mock_get):
+        mock_instance = Mock(spec=HubRequest)()
+        mock_instance.is_pending.return_value = False
+        mock_get.return_value = mock_instance
+        admin = HubRequestAdmin(HubRequest, self.site)
+        request = self.factory.get('/')
+        admin.approve_request(request, 1)
+
+    @patch_request_get
+    @patch('us_ignite.hubs.admin.HubApprovalRequestForm')
+    def test_request_admin_is_render_successfully(self, mock_form, mock_get):
+        mock_instance = Mock(spec=HubRequest)()
+        mock_instance.is_pending.return_value = True
+        mock_get.return_value = mock_instance
+        admin = HubRequestAdmin(HubRequest, self.site)
+        request = self.factory.get('/')
+        response = admin.approve_request(request, 1)
+        mock_get.assert_called_once_with(id=1)
+        mock_form.assert_called_once()
+        eq_(response.status_code, 200)
+        eq_(response.template_name, 'admin/hubs/request_approval.html')
+        eq_(sorted(response.context_data.keys()),
+            sorted(['object', 'form', 'title']))
+
+    def test_request_admin_is_approved(self):
+        hub_request = fixtures.get_hub_request()
+        admin = HubRequestAdmin(HubRequest, self.site)
+        request = self.factory.post('/', {'status': HubRequest.APPROVED})
+        request._messages = utils.TestMessagesBackend(request)
+        response = admin.approve_request(request, hub_request.id)
+        eq_(response.status_code, 302)
+        eq_(response['Location'],
+            '/admin/hubs/hubrequest/%s/' % hub_request.id)
+        instance = HubRequest.objects.get(id=hub_request.id)
+        ok_(instance.hub)
+        self._tear_down()
+
+
+class TestGetHubFromRequestAdmin(TestCase):
+
+    @patch('us_ignite.hubs.models.Hub.objects.create')
+    def test_creation_is_successful(self, mock_create):
+        user = User(id=1)
+        data = {
+            'name': 'Hello',
+            'user': user,
+            'summary': 'Summary',
+            'description': 'Description',
+            'website': 'http://us-ignite.org',
+        }
+        instance = HubRequest(**data)
+        get_hub_from_request(instance)
+        mock_create.assert_called_once_with(
+            name='Hello',
+            guardian=user,
+            summary='Summary',
+            description='Description',
+            website='http://us-ignite.org'
+        )
