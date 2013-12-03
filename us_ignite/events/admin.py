dad1160c48891b82c7512b196025e11e6c28721a
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-12-03 15:57:50 +0000

    Add admin section for the ``Events``.
    Useful for monitor and debug.

---
+++ us_ignite/events/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from us_ignite.events.models import Event
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'start_datetime', 'is_featured')
+    search_fields = ('name', 'slug', 'venue', 'description', 'notes')
+    list_filter = ('start_datetime', 'is_featured', 'created')
+    date_hierarchy = 'start_datetime'
+
+admin.site.register(Event, EventAdmin)
