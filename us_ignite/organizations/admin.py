98a11c892c1265d685d5f3da549ac8a07a17b482
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-12-06 23:17:15 +0000

    Add admin section for the ``Organization`` and ``Membership``.

---
+++ us_ignite/organizations/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from us_ignite.organizations.models import Organization, OrganizationMember
+
+
+class OrganizationMembershipInline(admin.TabularInline):
+    raw_id_fields = ('user', )
+    model = OrganizationMember
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'status', 'created')
+    search_fields = ('name', 'slug', 'bio')
+    list_filter = ('status', )
+    date_hierarchy = 'created'
+    inlines = [OrganizationMembershipInline]
+
+
+admin.site.register(Organization, OrganizationAdmin)
