96c866a27266ce93530c5cac5f59d59cc3e665ff
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-12-13 12:23:13 +0000

    Update ``challenge_entry`` description.

--- us_ignite/challenges/views.py
+++ us_ignite/challenges/views.py
@@ -21,13 +21,22 @@ def challenge_list(request):
 
 @login_required
 def challenge_entry(request, challenge_slug, app_slug):
+    """Entry form for an ``Application`` for a given ``Challenge``.
+
+    The ``Challenge`` has a list of ``Questions`` that will be translated
+    into a ``ChallengeForm``.
+
+    The ``owner`` of the ``Application`` can save the progress on the entry
+    decide when it's ready to participate."""
     challenge = get_object_or_404(Challenge.active, slug__exact=challenge_slug)
     application = get_object_or_404(
         Application.active, slug__exact=app_slug, owner=request.user,
         status=Application.PUBLISHED)
     # Generate a form from the questions in the admin:
     ChallengeForm = forms.get_challenge_form(challenge)
-    # TODO: determine what happens after the entry has been accepted.
+    # TODO: Once the entry has been marked as SUBMITTED or has been
+    # ACCEPTED the page shows the detail of the entry, unless withdrawn,
+    # and the entry would require moderation as well.
     if request.method == 'POST':
         # The entry is only created on a POST:
         entry, is_new = Entry.objects.get_or_create(
