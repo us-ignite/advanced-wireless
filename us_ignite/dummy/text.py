8e5c55a4710352d5f3b211c9df7d11c3cf9ef104
Alfredo Aguirre <alfredo@madewithbyt.es>
2013-12-16 14:54:35 +0000

    Handle encoding of the random words.

--- us_ignite/dummy/text.py
+++ us_ignite/dummy/text.py
@@ -1,14 +1,15 @@
 from random import choice
 
 from django.conf import settings
+from django.utils.encoding import smart_text
 
 
 words = open(settings.WORDS_PATH, "r").readlines()
 
 
 def random_words(total):
-    return " ".join([choice(words).lower().rstrip() for i in range(total)])
+    return u" ".join([smart_text(choice(words).lower().rstrip()) for i in range(total)])
 
 
 def random_paragraphs(total, word_no=30):
-    return ".\n\n".join([random_words(word_no) for i in range(total)])
+    return u".\n\n".join([random_words(word_no) for i in range(total)])
