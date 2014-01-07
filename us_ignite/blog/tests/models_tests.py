f2f834d829482af5dc53d8371c18d8fc7022bd99
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-01-07 14:30:52 +0000

    Add support for single digit months.
    Blog wouldn't render single digit months.

--- us_ignite/blog/tests/models_tests.py
+++ us_ignite/blog/tests/models_tests.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from nose.tools import eq_, ok_
 
 from django.contrib.auth.models import User
 from django.test import TestCase
+from django.utils import timezone
 
 from us_ignite.common.tests import utils
 from us_ignite.profiles.tests.fixtures import get_user
@@ -64,3 +66,19 @@ class TestEntryModel(TestCase):
         author = get_user('us-ignite')
         entry = fixtures.get_entry(author=author, status=Entry.DRAFT)
         eq_(entry.is_visible_by(author), True)
+
+    def test_get_absolute_url_single_digit_month(self):
+        author = get_user('us-ignite')
+        naive = datetime(2012, 3, 3, 1, 30)
+        publication_date = naive.replace(tzinfo=timezone.utc)
+        entry = fixtures.get_entry(
+            author=author, publication_date=publication_date, slug='gigabit-a')
+        eq_(entry.get_absolute_url(), '/blog/2012/3/gigabit-a/')
+
+    def test_get_absolute_url_double_digit_month(self):
+        author = get_user('us-ignite')
+        naive = datetime(2012, 10, 3, 1, 30)
+        publication_date = naive.replace(tzinfo=timezone.utc)
+        entry = fixtures.get_entry(
+            author=author, publication_date=publication_date, slug='gigabit-a')
+        eq_(entry.get_absolute_url(), '/blog/2012/10/gigabit-a/')
