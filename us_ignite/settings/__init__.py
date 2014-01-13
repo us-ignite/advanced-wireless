013697da4e1b9e99b033b52b2b57f54e3179ca03
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-01-13 18:11:01 +0000

    Add missing comma in settings.

--- us_ignite/settings/__init__.py
+++ us_ignite/settings/__init__.py
@@ -89,7 +89,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware'
+    'django.middleware.common.BrokenLinkEmailsMiddleware',
     'djangosecure.middleware.SecurityMiddleware',
     'us_ignite.common.middleware.DoNotTrackMiddleware',
 )
