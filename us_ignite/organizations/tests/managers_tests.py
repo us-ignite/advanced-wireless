52f47bf4fd0472752e3ac0bf03a8cd1a40325825
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-01-16 14:46:46 +0000

    Update cosmetic naming on the organization tests.

--- us_ignite/organizations/tests/managers_tests.py
+++ us_ignite/organizations/tests/managers_tests.py
@@ -11,10 +11,10 @@ class TestOrganizationActiveManager(TestCase):
     def tearDown(self):
         Organization.objects.all().delete()
 
-    def test_published_organizations_are_returned(self):
+    def test_active_organizations_are_returned(self):
         organization = fixtures.get_organization(status=Organization.PUBLISHED)
         eq_(list(Organization.active.all()), [organization])
 
-    def test_unpublished_organizations_are_not_returned(self):
+    def test_inactive_organizations_are_not_returned(self):
         organization = fixtures.get_organization(status=Organization.DRAFT)
         eq_(list(Organization.active.all()), [])
