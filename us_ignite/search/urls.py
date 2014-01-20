53878700a4da22e80114ef67a4aee340846abf91
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-01-20 15:29:53 +0000

    Fix broad regex for the ``search`` URLs.

--- us_ignite/search/urls.py
+++ us_ignite/search/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'us_ignite.search.views',
-    url(r'apps/', 'search_apps', name='search_apps'),
-    url(r'events/', 'search_events', name='search_events'),
-    url(r'hubs/', 'search_hubs', name='search_hubs'),
-    url(r'orgs/', 'search_organizations', name='search_organizations'),
-    url(r'resources/', 'search_resources', name='search_resources'),
+    url(r'^apps/$', 'search_apps', name='search_apps'),
+    url(r'^events/$', 'search_events', name='search_events'),
+    url(r'^hubs/$', 'search_hubs', name='search_hubs'),
+    url(r'^orgs/$', 'search_organizations', name='search_organizations'),
+    url(r'^resources/$', 'search_resources', name='search_resources'),
 )
