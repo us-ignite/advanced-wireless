db3e154e063aab984a3713d4e01fb2f071cc7fcb
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-03 13:06:02 +0000

    Improve the admin section for the ``Locations`` and ``Categories``.

--- us_ignite/maps/admin.py
+++ us_ignite/maps/admin.py
@@ -4,11 +4,15 @@ from us_ignite.maps.models import Category, Location
 
 
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug', 'image')
+    prepopulated_fields = {"slug": ("name",)}
 
 
 class LocationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'status', 'category')
+    search_fields = ('name', 'website', )
+    list_filter = ('status', 'category__name')
+    date_hierarchy = 'created'
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Location, LocationAdmin)
