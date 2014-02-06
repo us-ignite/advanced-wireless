a094bf3ae9acadfd81d3434bf40e78633060fed9
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-06 16:10:40 +0000

    Added ``status`` to the filtering in the ``events`` admin section.

--- us_ignite/events/admin.py
+++ us_ignite/events/admin.py
@@ -6,7 +6,7 @@ from us_ignite.events.models import Event
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'start_datetime', 'is_featured')
     search_fields = ('name', 'slug', 'venue', 'description', 'notes')
-    list_filter = ('start_datetime', 'is_featured', 'created')
+    list_filter = ('status', 'start_datetime', 'is_featured', 'created')
     date_hierarchy = 'start_datetime'
 
 admin.site.register(Event, EventAdmin)
