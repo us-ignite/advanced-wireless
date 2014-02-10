630ba21f3b08dcd2685297b057cbee4b6abee6f7
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-10 15:25:07 +0000

    Add help text describing the image field functionality.

--- us_ignite/sections/models.py
+++ us_ignite/sections/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Sponsor(models.Model):
     name = models.CharField(max_length=255)
     website = models.URLField(max_length=500)
-    image = models.ImageField(upload_to="sponsor")
+    image = models.ImageField(
+        upload_to="sponsor", help_text='This image is not post processed. '
+        'Please make sure it has the right design specs.')
     order = models.IntegerField(default=0)
 
     class Meta:
