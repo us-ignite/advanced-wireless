935b0402cd82b73cb8369acbb7ecfb0ae5ad13e5
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-16 14:23:07 +0000

    Add test helper to create inline forms payload.
    Usage:
    
        from us_ignite.common.tests import utils
        payload = utils.get_inline_payload(FormSetClass)
    
    Where the ``FormSetClass`` is an inline formset form. Optional
    keyword attributes are available:
    
    - ``instance`` instance for the payload.
    - ``data_list`` data to be bundled on every inline form.
    - ``max_num`` max number of forms.
    
     Any extra ``kwargs`` will be added to the form payload.

--- us_ignite/common/tests/utils.py
+++ us_ignite/common/tests/utils.py
@@ -35,6 +35,36 @@ def get_request(method, *args, **kwargs):
     return request
 
 
+def get_management_form(prefix, initial=0, total=0, max_num=3):
+    default = {
+        '%sTOTAL_FORMS' % prefix: total,
+        '%sINITIAL_FORMS' % prefix: initial,
+        '%sMAX_NUM_FORMS' % prefix: max_num,
+    }
+    return default
+
+
+def get_inline_payload(
+        formset_class, instance=None, data_list=None, max_num=3, **kwargs):
+    formset = formset_class(instance=instance)
+    data_list = data_list if data_list else []
+    prefix = '%s_set-' % formset.model.__name__.lower()
+    default = get_management_form(prefix, len(data_list), max_num)
+    _inline_tuple = lambda i, k, v: ('%s%s-%s' % (prefix, i, k), v)
+    instance_name = formset.instance.__class__.__name__.lower()
+    pk = '' if not instance else instance.pk
+    for i, inline in enumerate(data_list):
+        inline_default = {
+            '%s%s-DELETE' % (prefix, i): '',
+            '%s%s-%s' % (prefix, i, instance_name): pk,
+        }
+        inline_item = dict(_inline_tuple(i, k, v) for k, v in inline.items())
+        inline_default.update(inline_item)
+        default.update(inline_default)
+    default.update(kwargs)
+    return default
+
+
 class TestMessagesBackend(BaseStorage):
     """
     When unit testing a django view the ``messages`` middleware
