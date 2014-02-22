5b7e6caa85fa9fdd4ea085b4f082200e81cd1cdc
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-22 21:23:26 +0000

    Enable profile list section.

--- us_ignite/people/tests/integration_tests.py
+++ us_ignite/people/tests/integration_tests.py
@@ -13,6 +13,23 @@ def _teardown_profiles():
         model.objects.all().delete()
 
 
+class TestPeopleListUnauthenticated(TestCase):
+
+    def test_people_list_requires_auth(self):
+        url = '/people/'
+        response = self.client.get(url)
+        assert_redirects(response, utils.get_login_url(url))
+
+    def test_people_list_is_successful(self):
+        user = fixtures.get_user('us-ignite')
+        fixtures.get_profile(user=user, name='us ignite', slug='ignite')
+        self.client.login(username='us-ignite', password='us-ignite')
+        response = self.client.get('/people/')
+        eq_(response.status_code, 200)
+        self.client.logout()
+        _teardown_profiles()
+
+
 class TestPeopleDetailPage(TestCase):
 
     def test_people_page_detail_is_successful(self):
