7a02ee73e4d79aae786e76269cb4ec88d51483fc
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-22 14:15:17 +0000

    Add management command to add initial ``Resources`` fixtures.
    https://github.com/madewithbytes/us_ignite/issues/173

---
+++ us_ignite/resources/management/commands/resources_initial_fixtures.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand
+
+from us_ignite.resources.models import ResourceType, Sector
+
+
+RESOURCE_LIST = (
+    'Funding Opportunity',
+    'Video',
+    'Presentation',
+    'Tool',
+    'Testbed',
+    'External Group',
+    'Other',
+)
+
+SECTOR_LIST = (
+    'Public Safety',
+)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for resource_name in RESOURCE_LIST:
+            resource, is_new = (ResourceType.objects
+                                .get_or_create(name=resource_name))
+            if is_new:
+                print "Imported resource type: %s" % resource_name
+        for sector_name in SECTOR_LIST:
+            sector, is_new = Sector.objects.get_or_create(name=sector_name)
+            if is_new:
+                print "Imported sector: %s" % sector_name
+        print "Done!"
