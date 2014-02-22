32a9e3f0a4e442c95f926ccbae35077968876c42
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-22 13:20:59 +0000

    Update admin section for the ``Resources``.

--- us_ignite/resources/admin.py
+++ us_ignite/resources/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from us_ignite.resources.models import Resource
+from us_ignite.resources.models import Resource, ResourceType, Sector
 
 
 class ResourceAdmin(admin.ModelAdmin):
@@ -8,6 +8,17 @@ class ResourceAdmin(admin.ModelAdmin):
     search_fields = ('name', 'slug', 'description', 'url')
     list_filter = ('is_featured', 'created')
     date_hierarchy = 'created'
-    raw_id_fields = ['owner', ]
+    raw_id_fields = ['contact', 'author', ]
+
+
+class ResourceTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
 
 admin.site.register(Resource, ResourceAdmin)
+admin.site.register(ResourceType, ResourceTypeAdmin)
+admin.site.register(Sector, SectorAdmin)
