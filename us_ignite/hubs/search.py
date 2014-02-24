3b175fe6b0cbd020ba700ea4fcfa43778ceb0153
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-24 22:47:59 +0000

    Update search indexer to consider updated ``Hub`` fields.
    https://github.com/madewithbytes/us_ignite/issues/169

--- us_ignite/hubs/search.py
+++ us_ignite/hubs/search.py
@@ -14,7 +14,13 @@ class HubSearchAdapter(watson.SearchAdapter):
     def get_content(self, obj):
         fields = [
             obj.description,
+            obj.connections,
+            obj.estimated_passes,
             ', '.join([f.name for f in obj.features.all()]),
             ', '.join([t.name for t in obj.tags.all()]),
         ]
+        if obj.network_speed:
+            fields.append(obj.network_speed.name)
+        if obj.organization:
+            fields.append(obj.organization.name)
         return render.render_fields(fields)
