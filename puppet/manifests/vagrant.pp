5dfd1b04c2ab5a41a87af5b238bd9f32568619f9
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-03-01 09:41:58 +0000

    Update ``vagrant`` provisioning manifest.
    The update separates the update/upgrade mechanics
    to its own manifest file.
    
    Also makes sure that the latest version of nodejs is
    installed as well as any other tool required by the
    front end.

--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -14,49 +14,74 @@ Exec {
 }
 
 class dev {
+
   class {
-    init: ;
-    memcached: ;
-    locales: ;
+    update: ;
+
   }
+
+  class { "init":
+    require => Class[update],
+  }
+
+  class { "memcached":
+    require => Class[update],
+  }
+
+  class { "locales":
+    require => Class[update],
+  }
+
   class { "versioning":
-    require => [Class[init], Class[locales]],
+    require => Class[update],
   }
+
+  class { "nodejs":
+    project_path => $project_path,
+  }
+
   class { "postsql":
     require => Class[versioning],
     username => $username,
     password => $password,
     project_name => $project_name;
   }
+
   class { "python":
     require => Class[postsql],
     project_path => $project_path;
   }
+
   class { "certificates":
     require => Class[python],
     server_name => $server_name;
   }
+
   class { "nginx":
     require => Class[certificates],
     server_name => $server_name,
     project_name => $project_name,
     project_path => $project_path;
   }
+
   class { "application":
     require => Class[nginx],
     project_path => $project_path,
     project_name => $project_name,
   }
+
   class { "procfile":
     require => Class[application],
     project_name => $project_name,
     project_path => $project_path;
   }
+
   class { "custom":
     require => Class[procfile],
     project_path => $project_path,
     project_name => $project_name;
   }
+
 }
 
 include dev
