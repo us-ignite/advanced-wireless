14c23a20ea01ccadbb073a45d269aa53e4356c63
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-03-02 20:11:09 +0000

    Upgrade packages only when notified.

--- puppet/manifests/classes/updates.pp
+++ puppet/manifests/classes/updates.pp
@@ -10,6 +10,7 @@ class updates {
   exec { "updates_dist_upgrade":
     command => "apt-get -y dist-upgrade",
     logoutput => 'on_failure',
+    refreshonly => true,
     notify => Exec["updates_apt_update"],
     timeout => 10000,
   }
