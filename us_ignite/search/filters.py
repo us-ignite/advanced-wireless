a4846694a5553ef6db170574914a0b540e83ddb1
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-03-09 19:35:30 +0000

    Use postgres full-text search for tag searching.
    https://github.com/madewithbytes/us_ignite/issues/185
    
    Replacing the tag queryset with the full-text search provides
    better results, while maintaining the filtering by object type.

--- us_ignite/search/filters.py
+++ us_ignite/search/filters.py
@@ -7,15 +7,17 @@ from us_ignite.search.forms import TagSearchForm
 
 
 def tag_search(request, klass, template):
+    """Search subset of models with the given tag.
+
+    Usess ``watson`` for the full-text search.
+    """
     # Generate a queryset from a model, manager or queryset:
     form = TagSearchForm(request.GET)
     page_no = pagination.get_page_no(request.GET)
     if form.is_valid():
         queryset = get_queryset(klass)
-        object_list = queryset.filter(tags__name=form.cleaned_data['tag'])
-        # TODO: consider using watson search backend:
-        # object_list = watson.search(
-        #     form.cleaned_data['tag'], models=(queryset, ))
+        object_list = watson.search(
+            form.cleaned_data['tag'], models=(queryset, ))
     else:
         object_list = []
     page = pagination.get_page(object_list, page_no)
--- us_ignite/search/tests/filters_tests.py
+++ us_ignite/search/tests/filters_tests.py
@@ -8,32 +8,34 @@ from us_ignite.search import filters
 from us_ignite.common.tests import utils
 
 
-qs_patch = patch('us_ignite.search.filters.get_queryset')
+qs_patch = patch('watson.search')
+get_qs_patch = patch('us_ignite.search.filters.get_queryset')
 
 
 class TestTagSearchView(TestCase):
 
-    @qs_patch
-    def test_tag_search_has_no_query(self, qs_mock):
+    @get_qs_patch
+    def test_tag_search_has_no_url_query(self, get_qs_mock):
+        get_qs_mock.return_value = []
         request = utils.get_request('get', '/search/apps/')
         response = filters.tag_search(
             request, Model, template='tag_list.html')
         eq_(response.status_code, 200)
         eq_(response.template_name, 'tag_list.html')
         eq_(sorted(response.context_data.keys()), ['form', 'page'])
-        eq_(qs_mock.call_count, 0)
+        eq_(get_qs_mock.call_count, 0)
 
     @qs_patch
-    def test_search_tags_search(self, qs_mock):
-        qs_instance = Mock()
-        qs_instance.filter.return_value = []
-        qs_mock.return_value = qs_instance
+    @get_qs_patch
+    def test_search_tags_search(self, get_qs_mock, qs_mock):
+        get_qs_mock.return_value = []
+        qs_mock.return_value = []
         data = {'tag': 'test'}
         request = utils.get_request('get', '/search/apps/', data=data)
         response = filters.tag_search(
             request, Model, template='tag_list.html')
-        qs_mock.assert_called_once_with(Model)
-        qs_instance.filter.assert_called_once_with(tags__name='test')
+        get_qs_mock.assert_called_once_with(Model)
+        qs_mock.assert_called_once_with('test', models=([], ))
         eq_(response.status_code, 200)
         eq_(response.template_name, 'tag_list.html')
         eq_(sorted(response.context_data.keys()), ['form', 'page'])
