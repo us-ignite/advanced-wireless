32f0568bd598612bea18ac4a5c2f318198cc2664
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-03-17 22:02:09 +0000

    Unify search by tag with the search results.
    https://github.com/madewithbytes/us_ignite/issues/185
    
    This narrows the search to only selected items of the
    same category, but using the same mechanics as the
    regular search.

--- us_ignite/search/filters.py
+++ us_ignite/search/filters.py
@@ -1,9 +1,10 @@
 import watson
 from django.template.response import TemplateResponse
 from django.shortcuts import _get_queryset as get_queryset
+from django.utils.http import urlencode
 
 from us_ignite.common import pagination
-from us_ignite.search.forms import TagSearchForm
+from us_ignite.search.forms import SearchForm
 
 
 def tag_search(request, klass, template):
@@ -12,17 +13,22 @@ def tag_search(request, klass, template):
     Usess ``watson`` for the full-text search.
     """
     # Generate a queryset from a model, manager or queryset:
-    form = TagSearchForm(request.GET)
+    form = SearchForm(request.GET)
     page_no = pagination.get_page_no(request.GET)
     if form.is_valid():
         queryset = get_queryset(klass)
         object_list = watson.search(
-            form.cleaned_data['tag'], models=(queryset, ))
+            form.cleaned_data['q'], models=(queryset, ))
+        pagination_qs = '&%s' % urlencode({'q': form.cleaned_data['q']})
     else:
         object_list = []
+        pagination_qs = ''
     page = pagination.get_page(object_list, page_no)
+    page.object_list_top = [o.object for o in page.object_list_top]
+    page.object_list_bottom = [o.object for o in page.object_list_bottom]
     context = {
         'page': page,
         'form': form,
+        'pagination_qs': pagination_qs,
     }
     return TemplateResponse(request, template, context)
--- us_ignite/search/tests/filters_tests.py
+++ us_ignite/search/tests/filters_tests.py
@@ -1,4 +1,4 @@
-from mock import Mock, patch
+from mock import patch
 from nose.tools import eq_
 
 from django.db.models import Model
@@ -22,7 +22,8 @@ class TestTagSearchView(TestCase):
             request, Model, template='tag_list.html')
         eq_(response.status_code, 200)
         eq_(response.template_name, 'tag_list.html')
-        eq_(sorted(response.context_data.keys()), ['form', 'page'])
+        eq_(sorted(response.context_data.keys()),
+            sorted(['form', 'page', 'pagination_qs']))
         eq_(get_qs_mock.call_count, 0)
 
     @qs_patch
@@ -30,7 +31,7 @@ class TestTagSearchView(TestCase):
     def test_search_tags_search(self, get_qs_mock, qs_mock):
         get_qs_mock.return_value = []
         qs_mock.return_value = []
-        data = {'tag': 'test'}
+        data = {'q': 'test'}
         request = utils.get_request('get', '/search/apps/', data=data)
         response = filters.tag_search(
             request, Model, template='tag_list.html')
@@ -38,4 +39,5 @@ class TestTagSearchView(TestCase):
         qs_mock.assert_called_once_with('test', models=([], ))
         eq_(response.status_code, 200)
         eq_(response.template_name, 'tag_list.html')
-        eq_(sorted(response.context_data.keys()), ['form', 'page'])
+        eq_(sorted(response.context_data.keys()),
+            sorted(['form', 'page', 'pagination_qs']))
