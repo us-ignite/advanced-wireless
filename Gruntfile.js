48d72e5b9702c06e7882b678babc31cb0632b0c8
Ben Barnett <ben.barnett@moneyadviceservice.org.uk>
2014-03-19 11:11:20 +0000

    pulling down basic includes for ie to local and introducing grunt tasks #222

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,59 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    meta: {
+      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      modern: [
+        'us_ignite/assets/bower_components/jquery/dist/jquery.min.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.dropdown.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.orbit.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.topbar.js',
+        'us_ignite/assets/js/vendor/jquery-ui-1.10.4.custom.js',
+        'us_ignite/assets/js/app.js'
+        ],
+      basic: [
+        'us_ignite/assets/bower_components/jquery-1.9.1/index.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.dropdown.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.orbit.js',
+        'us_ignite/assets/bower_components/foundation/js/foundation/foundation.topbar.js',
+        'us_ignite/assets/js/vendor/jquery-ui-1.10.4.custom.js',
+        'us_ignite/assets/js/vendor/html5shiv.js',
+        'us_ignite/assets/js/vendor/nwmatcher-1.2.5-min.js',
+        'us_ignite/assets/js/vendor/selectivizr-1.0.3b.js',
+        'us_ignite/assets/js/vendor/respond.min.js',
+        'us_ignite/assets/js/app.js'
+        ]
+    },
+
+    concat: {
+      options: {
+        separator: '\n;\n',
+        banner: '<%= meta.banner %>'
+      },
+      all: {
+        files: {
+          'us_ignite/assets/js/lib/modern.js': ['<%= meta.modern %>'],
+          'us_ignite/assets/js/lib/basic.js': ['<%= meta.basic %>']
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: {
+          except: []
+        }
+      },
+      all: {
+        files: {
+          'us_ignite/assets/js/lib/modern.js': 'us_ignite/assets/js/lib/modern.js',
+          'us_ignite/assets/js/lib/basic.js': 'us_ignite/assets/js/lib/basic.js'
+        }
+      }
+    },
+
     sass: {
       options: {
         includePaths: ['us_ignite/assets/bower_components/foundation/scss']
@@ -42,6 +95,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-pixrem');
 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask('build', ['sass', 'pixrem']);
-  grunt.registerTask('default', ['build','watch']);
+  grunt.registerTask('buildjs', ['concat', 'uglify']);
+  grunt.registerTask('default', ['build','buildjs', 'watch']);
 }
