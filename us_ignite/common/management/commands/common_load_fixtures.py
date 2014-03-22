bdeb60d5e82e5eaaaaf805286bae29e9112af307
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-03-22 21:53:17 +0000

    Add initial data for the ``Interest`` model.

--- us_ignite/common/management/commands/common_load_fixtures.py
+++ us_ignite/common/management/commands/common_load_fixtures.py
@@ -4,6 +4,24 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
 
+from us_ignite.profiles.models import Interest
+
+
+INTEREST_LIST = (
+    ('SDN', 'sdn'),
+    ('OpenFlow', 'openflow'),
+    ('Ultra fast', 'ultra-fast'),
+    ('Advanced wireless', 'advanced-wireless'),
+    ('Low-latency', 'low-latency'),
+    ('Local cloud/edge computing', 'local-cloud-edge-computing'),
+    ('Healthcare', 'healthcare'),
+    ('Education & Workforce development', 'education-workforce-development'),
+    ('Energy', 'energy'),
+    ('Transportation', 'transportation'),
+    ('Advanced Manufacturing', 'advanced-manufacturing'),
+    ('Public Safety', 'public-safety'),
+)
+
 
 class Command(BaseCommand):
 
@@ -11,4 +29,9 @@ class Command(BaseCommand):
         parsed_url = urlparse.urlparse(settings.SITE_URL)
         Site.objects.all().update(domain=parsed_url.netloc,
                                   name=parsed_url.netloc)
-        print "Done!"
+        print "Updated site URL."
+        for name, slug in INTEREST_LIST:
+            interest, is_new = (Interest.objects
+                                .get_or_create(name=name, slug=slug))
+            if is_new:
+                print u'Imported interest: %s' % interest
