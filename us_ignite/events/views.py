d7ab6160b6a471c942ac6c071cd7613cc9ec01f0
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-05-25 15:00:27 +0200

    Update calendar export information.
    https://github.com/madewithbytes/us_ignite/issues/288
    
    The calendar preserves the timezone of the event created
    it also adds relevant information to the ``ics`` file
    such as location, description and URL.

--- us_ignite/events/views.py
+++ us_ignite/events/views.py
@@ -1,5 +1,6 @@
 import pytz
 
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponse
@@ -7,7 +8,7 @@ from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.utils import timezone
 
-from icalendar import Calendar, Event as CalEvent
+from icalendar import Calendar, vText, Event as CalEvent
 from us_ignite.common import pagination
 from us_ignite.events.forms import EventForm, EventURLFormSet
 from us_ignite.events.models import Event
@@ -30,22 +31,39 @@ def event_detail(request, slug):
     return TemplateResponse(request, 'events/object_detail.html', context)
 
 
+def _swap_timezone(timezone_name):
+    TIMEZONE_EQUIVALENT = {
+        'US/Eastern': 'America/New_York',
+    }
+    if timezone_name in TIMEZONE_EQUIVALENT:
+        return TIMEZONE_EQUIVALENT[timezone_name]
+    return timezone_name
+
+
 def event_detail_ics(request, slug):
     event_obj = get_object_or_404(Event.published, slug__exact=slug)
     calendar = Calendar()
     calendar.add('prodid', '-//US Ignite//us-ignite.org//')
-    calendar.add('version', '1.0')
+    calendar.add('version', '2.0')
     event = CalEvent()
     event.add('summary', event_obj.name)
-    event.add('dtstart', event_obj.start_datetime)
+    url = '%s%s' % (settings.SITE_URL, event_obj.get_absolute_url())
+    description = event_obj.description + '\n\n' + url
+    event.add('description', description)
+    event.add('url', url)
+    event['location'] = vText(event_obj.address)
+    # Use timezone aware datetime objects:
+    event_tz = pytz.timezone(_swap_timezone(event_obj.timezone))
+    event.add('dtstart', event_obj.start_datetime.astimezone(event_tz))
     if event_obj.end_datetime:
-        event.add('dtend', event_obj.end_datetime)
+        event.add('dtend', event_obj.end_datetime.astimezone(event_tz))
     event.add('dtstamp', timezone.now())
-    event['uid'] = '%s/%s@us-ignite.org' % (event_obj.pk, timezone.now())
+    event['uid'] = 'event-%s/@us-ignite.org' % (event_obj.pk)
     event.add('priority', 5)
     calendar.add_component(event)
+    file_name = 'event-%s.ics' % event_obj.slug
     response = HttpResponse(calendar.to_ical(), content_type='text/calendar')
-    response['Content-Disposition'] = 'attachment; filename="event.ics"'
+    response['Content-Disposition'] = 'attachment; filename="%s"' % file_name
     return response
 
 
