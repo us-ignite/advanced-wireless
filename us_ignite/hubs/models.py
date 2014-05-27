6b28ec2fd6c96bee92c6956e3afba44542f060da
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-05-27 20:03:33 +0200

    Removed ``Testbeds`` fields from ``Hubs``.
    https://github.com/madewithbytes/us_ignite/issues/294
    
    The fields that belong to a ``Testbed`` were removed
    from the ``Hub`` model.
    
    These fields will be reimplemented in the ``testbed`` app.

--- us_ignite/hubs/models.py
+++ us_ignite/hubs/models.py
@@ -57,14 +57,6 @@ class HubRequest(models.Model):
         return (self.status == self.PENDING) and not self.hub
 
 
-class NetworkSpeed(models.Model):
-    name = models.CharField(max_length=255)
-    slug = AutoSlugField(populate_from='name', unique=True)
-
-    def __unicode__(self):
-        return self.name
-
-
 class Hub(models.Model):
     """Local communities with Gigabit capabilities."""
     PUBLISHED = 1
@@ -73,39 +65,14 @@ class Hub(models.Model):
         (PUBLISHED, u'Published'),
         (DRAFT, u'Draft'),
     )
-    LOW = 1
-    MEDIUM = 2
-    HIGH = 3
-    EXPERIMENTATION_CHOICES = (
-        (LOW, u'Low'),
-        (MEDIUM, u'Medium'),
-        (HIGH, u'High'),
-    )
     name = models.CharField(max_length=255)
     slug = AutoSlugField(populate_from='name', unique=True)
     summary = models.TextField(blank=True)
     description = models.TextField()
-    connections = models.TextField(
-        blank=True, help_text=u'Connections to other networks')
     contact = models.ForeignKey(
         'auth.User', blank=True, null=True, on_delete=models.SET_NULL)
-    organization = models.ForeignKey(
-        'organizations.Organization', blank=True, null=True,
-        on_delete=models.SET_NULL)
-    network_speed = models.ForeignKey(
-        'hubs.NetworkSpeed', blank=True, null=True, on_delete=models.SET_NULL)
-    is_advanced = models.BooleanField(
-        default=False, help_text=u'Does it have advanced characteristics?')
-    experimentation = models.IntegerField(
-        choices=EXPERIMENTATION_CHOICES, default=MEDIUM,
-        help_text=u'Willingness to experiment')
-    estimated_passes = models.TextField(
-        blank=True, help_text=u'# homes, # businesses, # community anchor '
-        'institutions')
     image = models.ImageField(blank=True, upload_to='hub', max_length=500)
     website = models.URLField(max_length=500, blank=True)
-    applications = models.ManyToManyField(
-        'apps.Application', blank=True, help_text=u'Applications being piloted.')
     features = models.ManyToManyField(
         'apps.Feature', blank=True, help_text=u'Existing NextGen features in '
         'this community.')
