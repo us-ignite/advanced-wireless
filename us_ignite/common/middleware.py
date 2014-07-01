348f713f110ffac0c49529a98ee8127f0b4c4db6
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-07-01 22:18:37 +0100

    Add middlware to redirect valid HOST requests.
    The middleware will redirect accepted HTTP_HOSTS
    to the specified SITE_URL.

--- us_ignite/common/middleware.py
+++ us_ignite/common/middleware.py
@@ -1,4 +1,20 @@
+import urlparse
+from django.http import HttpResponsePermanentRedirect
+from django.conf import settings
+
+
 class DoNotTrackMiddleware(object):
 
     def process_request(self, request):
         request.is_dnt = request.META.get('HTTP_DNT') == '1'
+
+
+class URLRedirectMiddleware(object):
+    """Redirects an invalid HOST URL to the expected domain."""
+
+    def process_request(self, request):
+        host = request.META.get('HTTP_HOST', '')
+        parsed_url = urlparse.urlparse(settings.SITE_URL)
+        if not host == parsed_url.netloc:
+            new_url = settings.SITE_URL + request.path_info
+            return HttpResponsePermanentRedirect(new_url)
--- us_ignite/common/tests/middleware_tests.py
+++ us_ignite/common/tests/middleware_tests.py
@@ -1,5 +1,6 @@
 from nose.tools import eq_
 
+from django.conf import settings
 from django.test import TestCase, client
 
 from us_ignite.common import middleware
@@ -29,3 +30,24 @@ class TestDoNotTrackMiddleware(TestCase):
         instance = middleware.DoNotTrackMiddleware()
         instance.process_request(request)
         eq_(request.is_dnt, False)
+
+
+class TestURLRedirectMiddleware(TestCase):
+
+    def setUp(self):
+        self.factory = client.RequestFactory()
+
+    def test_middleware_lets_through_right_host(self):
+        request = self.factory.get('/')
+        request.META['HTTP_HOST'] = 'testing-us-ignite.org'
+        instance = middleware.URLRedirectMiddleware()
+        response = instance.process_request(request)
+        eq_(response, None)
+
+    def test_middleware_redirects_other_hosts(self):
+        request = self.factory.get('/some-url/')
+        request.META['HTTP_HOST'] = 'www.us-ignite.org'
+        instance = middleware.URLRedirectMiddleware()
+        response = instance.process_request(request)
+        eq_(response.status_code, 301)
+        eq_(response['Location'], 'http://testing-us-ignite.org/some-url/')
