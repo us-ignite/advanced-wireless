54f203a1e6325b3c712778703179356134d33dff
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-07-06 22:07:54 +0100

    Add admin section to the ``StaticPage``.

--- us_ignite/sections/admin.py
+++ us_ignite/sections/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from us_ignite.sections.models import Sponsor
+from us_ignite.sections.models import Sponsor, SectionPage
 
 
 class SponsorAdmin(admin.ModelAdmin):
     list_display = ['name', 'order']
 
 
+class SectionPageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'status']
+    search_fields = ['title', 'slug', 'body', 'status', 'template']
+    list_filter = ['status']
+
 admin.site.register(Sponsor, SponsorAdmin)
+admin.site.register(SectionPage, SectionPageAdmin)
