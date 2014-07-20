6e0969a6707c023b45638fcc90d62681d4a72d0f
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-07-20 21:29:38 +0100

    Remove unused admin events option.

--- us_ignite/hubs/admin.py
+++ us_ignite/hubs/admin.py
@@ -74,14 +74,5 @@ class HubAdmin(admin.ModelAdmin):
     date_hierarchy = 'created'
     list_filter = ('status', 'created')
 
-
-class HubActivityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created')
-    search_fields = ('name', 'description')
-    date_hierarchy = 'created'
-    list_filter = ('created', )
-
-
 admin.site.register(Hub, HubAdmin)
-admin.site.register(HubActivity, HubActivityAdmin)
 admin.site.register(HubRequest, HubRequestAdmin)
