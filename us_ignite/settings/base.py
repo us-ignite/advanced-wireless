f8130a858089844aa9c1cf8200bed24cfff6749e
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-08-05 07:58:40 +0100

    Reduce noise reporting.

--- us_ignite/settings/base.py
+++ us_ignite/settings/base.py
@@ -107,7 +107,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.common.BrokenLinkEmailsMiddleware',
+    # 'django.middleware.common.BrokenLinkEmailsMiddleware',
     'django.contrib.redirects.middleware.RedirectFallbackMiddleware',
     'djangosecure.middleware.SecurityMiddleware',
     'us_ignite.common.middleware.DoNotTrackMiddleware',
