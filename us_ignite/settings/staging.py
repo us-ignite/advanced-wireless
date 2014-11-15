51f87ed97e91b699dec8ddd1e281beb569e882d1
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-11-15 10:49:29 +0000

    Enable compressor.

--- us_ignite/settings/staging.py
+++ us_ignite/settings/staging.py
@@ -97,7 +97,7 @@ MAILCHIMP_LIST = env('MAILCHIMP_LIST')
 GOOGLE_ANALYTICS_ID = 'DUMMY'
 
 # Asset compressor:
-COMPRESS_ENABLED = False
+COMPRESS_ENABLED = True
 STATIC_FILES_VERSION = 'v1'
 # Heroku does not have a filesystem, used to deploy the assets to S3:
 #COMPRESS_STORAGE = 'us_ignite.common.storage.CachedS3BotoStorage'
