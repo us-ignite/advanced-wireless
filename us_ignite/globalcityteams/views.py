5975e4a94895d9e2bf301c6f27be19e0d0ee56c5
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-11-25 22:48:58 +0000

    Implement news section for the global city teams.

--- us_ignite/globalcityteams/views.py
+++ us_ignite/globalcityteams/views.py
@@ -1,3 +1,8 @@
+from us_ignite.blog.models import Post
+from us_ignite.blog.views import (
+    post_list as post_list_source,
+    post_detail as post_detail_source,
+)
 from us_ignite.events.models import Event
 from us_ignite.events.views import event_list as event_list_source
 from us_ignite.search.views import search as search_source
@@ -11,5 +16,22 @@ def event_list(request):
 
 
 def search(request):
+    """Search for the global city teams content."""
     response = search_source(request, 'globalcities')
+    response.template_name = 'globalcityteams/search.html'
+    return response
+
+
+def post_list(request):
+    """News for the global city teams"""
+    response = post_list_source(request, section=Post.GLOBALCITIES)
+    response.template_name = 'globalcityteams/news_list.html'
+    return response
+
+
+def post_detail(request, year, month, slug):
+    """News detail the global city teams"""
+    response = post_detail_source(
+        request, year, month, slug, section=Post.GLOBALCITIES)
+    response.template_name = 'globalcityteams/news_detail.html'
     return response
