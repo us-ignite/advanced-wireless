1351ca00d99318695b0302edbe19c037952a65fe
Walter Lau <walter@goodmorningstudio.com>
2015-05-08 13:13:58 -0700

    adding globalcityteamsfestival to globalcityteamsexpo redirect

--- us_ignite/globalcityteams/urls.py
+++ us_ignite/globalcityteams/urls.py
@@ -26,7 +26,5 @@ urlpatterns = patterns(
         'post_detail', name='news_detail'),
     url(r'^subscribe/$', 'mailing_subscribe', name='subscribe'),
     url(r'^TechJam2015?/$', RedirectView.as_view(
-        url='https://s3.amazonaws.com/us-ignite-org/static/pdf/Global+City+Teams+TECH+JAM+Prelim+Agenda.pdf'), 
-    
-        name='tech_jam_2015_pdf'),
+        url='https://s3.amazonaws.com/us-ignite-org/static/pdf/Global+City+Teams+TECH+JAM+Prelim+Agenda.pdf'), name='tech_jam_2015_pdf'),
 )
