8b5d259c74b1b6eccc30f46f57ce3296e330ab9b
George Holmes <george@georgeholmesii.com>
2016-07-07 17:30:10 -0500

    Updated form and logos on AWT page

--- us_ignite/advanced_wireless_testbed/forms.py
+++ us_ignite/advanced_wireless_testbed/forms.py
@@ -1,11 +1,7 @@
 from django import forms
 
-
-ATTRS = {
-    'style': 'width:10em;'
-}
 class EmailForm(forms.Form):
-    firstname = forms.CharField(required=True, widget=forms.TextInput(attrs={'cols': 100, 'style': 'width: 63em;'}))
-    lastname = forms.CharField(required=True, widget=forms.TextInput(attrs=ATTRS))
-    organization = forms.CharField(required=False, widget=forms.TextInput(attrs={'style': 'width: 63em;'}))
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'style': 'width: 63em;'}))
+    firstname = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'First Name', 'required': ''}))
+    lastname = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'required': ''}))
+    organization = forms.CharField(required=False, widget=forms.TextInput(attrs={'placeholder': 'Organization', 'required': ''}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'placeholder': 'Email Address', 'required': ''}))
