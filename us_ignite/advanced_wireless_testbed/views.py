44b99401af2ac1aad916727833ac3512710070d4
Walter Lau <walter@fissionstrategy.com>
2016-07-07 11:51:03 -0700

    updating awt form

--- us_ignite/advanced_wireless_testbed/views.py
+++ us_ignite/advanced_wireless_testbed/views.py
@@ -1,6 +1,6 @@
 from django.template.response import TemplateResponse
 from us_ignite.mailinglist.views import *
-from us_ignite.mailinglist.forms import EmailForm
+from forms import EmailForm
 
 def awt_frontpage(request):
     form = EmailForm()
