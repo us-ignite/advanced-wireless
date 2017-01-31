9dc7df4523b652328d1db78f7c4b5cf5d29a4943
George Holmes <george@georgeholmesii.com>
2017-01-31 17:57:15 -0600

    Updated reverse pitch page

--- us_ignite/assets/js/sgc.js
+++ us_ignite/assets/js/sgc.js
@@ -1,3 +1,13 @@
 (function ($) {
-	
+	$( "#menu-button" ).click(function() {     
+		var $elems = $('.jump-to-container, .social-bar');
+		if ( $elems.hasClass("active"))
+		{
+			$elems.removeClass("active");
+		}
+		else
+		{
+			$elems.addClass("active");
+		}
+	});
 })(jQuery);
