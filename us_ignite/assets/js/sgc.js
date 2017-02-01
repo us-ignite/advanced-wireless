3d3a65f76c626ffae9a22b9115e9c125168785c7
George Holmes <george@georgeholmesii.com>
2017-02-01 01:42:11 -0600

    Updated reverse pitch page pitches list

--- us_ignite/assets/js/sgc.js
+++ us_ignite/assets/js/sgc.js
@@ -1,13 +1,18 @@
 (function ($) {
-	$( "#menu-button" ).click(function() {     
-		var $elems = $('.jump-to-container, .social-bar');
-		if ( $elems.hasClass("active"))
-		{
-			$elems.removeClass("active");
-		}
-		else
-		{
-			$elems.addClass("active");
-		}
+	$(function () {
+		$( "#menu-button" ).click(function() {     
+			var $elems = $('.jump-to-container, .social-bar');
+			if ( $elems.hasClass("active"))
+			{
+				$elems.removeClass("active");
+			}
+			else
+			{
+				$elems.addClass("active");
+			}
+		});
+
+		$("div.video").fitVids();
 	});
+
 })(jQuery);
