9f712975864e283f1458e4ce5c7174c807641daf
Walter Lau <walter@fissionstrategy.com>
2017-04-18 11:54:26 -0700

    add lockdown

--- us_ignite/settings/base.py
+++ us_ignite/settings/base.py
@@ -112,7 +112,6 @@ MIDDLEWARE_CLASSES = (
     'djangosecure.middleware.SecurityMiddleware',
     'us_ignite.common.middleware.DoNotTrackMiddleware',
     'us_ignite.common.middleware.URLRedirectMiddleware',
-    # 'lockdown.middleware.LockdownMiddleware',
 )
 
 
